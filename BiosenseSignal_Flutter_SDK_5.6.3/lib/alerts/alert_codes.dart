4b010a092912ff08453be99b9374b92ad6530626
Md. Yousuf Ali <myali@Yousufs-MacBook-Pro.local>
2024-11-14 16:45:57 +0600

    chores: remove alert_code from .gitignore file

---
+++ BiosenseSignal_Flutter_SDK_5.6.3/lib/alerts/alert_codes.dart
@@ -0,0 +1,65 @@
+
+class AlertCodes {
+
+	static const int deviceCodeLowPowerModeEnabledError = 4;
+	static const int deviceCodeMinimumOsVersionError = 8;
+	static const int deviceCodeMinimumBatteryLevelError = 14;
+	static const int deviceCodeClockSkewError = 17;
+	static const int deviceBluetoothIsOffError = 19;
+	static const int deviceBluetoothMissingPermissionsError = 20;
+	static const int deviceLocationIsOffError = 21;
+	static const int cameraCodeNoCameraError = 1001;
+	static const int cameraCodeCameraOpenError = 1002;
+	static const int cameraCodeCameraMissingPermissionsError = 1005;
+	static const int cameraCodeUnexpectedImageDimensionsWarning = 1501;
+	static const int licenseCodeActivationLimitReachedError = 2002;
+	static const int licenseCodeMeterAttributeUsesLimitReachedError = 2003;
+	static const int licenseCodeAuthenticationFailedError = 2004;
+	static const int licenseCodeInvalidLicenseKeyError = 2007;
+	static const int licenseCodeRevokedLicenseError = 2010;
+	static const int licenseCodeInternalError9 = 2016;
+	static const int licenseCodeLicenseExpiredError = 2017;
+	static const int licenseCodeLicenseSuspendedError = 2018;
+	static const int licenseCodeNetworkIssuesError = 2024;
+	static const int licenseCodeSslHandshakeError = 2025;
+	static const int licenseCodeInputLicenseKeyEmptyError = 2032;
+	static const int licenseCodeInputProductIdIllegalError = 2034;
+	static const int licenseCodeCannotOpenFileForReadError = 2035;
+	static const int licenseCodeMonthlyUsageTrackingRequiresSyncError = 2036;
+	static const int licenseCodeSslHandshakeDeviceDateError = 2037;
+	static const int licenseCodeSslHandshakeCertificateExpiredError = 2038;
+	static const int licenseCodeMinSdkError = 2039;
+	static const int licenseCodeNetworkTimeoutError = 2042;
+	static const int licenseCodeFallDetectionDisabledError = 2049;
+	static const int measurementCodeMisdetectionDurationExceedsLimitError = 3003;
+	static const int measurementCodeInvalidRecentDetectionRateError = 3004;
+	static const int measurementCodeLicenseActivationFailedError = 3006;
+	static const int measurementCodeInvalidMeasurementAverageDetectionRateError = 3008;
+	static const int measurementCodeTooManyFramesInorderError = 3009;
+	static const int measurementCodeMisdetectionDurationExceedsLimitWarning = 3500;
+	static const int measurementCodeInvalidRecentFpsRateWarning = 3505;
+	static const int measurementCodeMeasurementMisplacedFrameWarning = 3506;
+	static const int vitalSignCodeBloodPressureProcessingFailedWarning = 4505;
+	static const int vitalSignCodeMeasuringWithNoEnabledVitalSignsWarning = 4506;
+	static const int sessionCodeIllegalStartCallError = 6004;
+	static const int sessionCodeIllegalStopCallError = 6005;
+	static const int initializationCodeInvalidProcessingTimeError = 7002;
+	static const int initializationCodeRotationAndOrientationMismatch = 7004;
+	static const int initializationCodeInvalidLicenseFormat = 7005;
+	static const int initializationCodeSdkLoadFailure = 7006;
+	static const int initializationCodeUnsupportedUserWeight = 7007;
+	static const int initializationCodeUnsupportedUserAge = 7008;
+	static const int initializationCodeConcurrentSessionsError = 7009;
+	static const int initializationMissingPolarDependenciesError = 7010;
+	static const int initializationMissingAnalyticsDependenciesError = 7011;
+	static const int initializationCodeUnsupportedUserHeight = 7012;
+	static const int ppgDeviceConnectionTimeoutError = 8003;
+	static const int ppgDeviceInvalidDeviceIdError = 8004;
+	static const int ppgDeviceUnsupportedFirmwareVersionError = 8005;
+	static const int ppgDeviceUnsupportedDeviceModelError = 8006;
+	static const int ppgDeviceConnectionError = 8007;
+	static const int ppgDeviceMisplacementError = 8008;
+	static const int monitoringStoppedError = 9001;
+	static const int monitoringDataGapExceedsLimitWarning = 9500;
+
+}
